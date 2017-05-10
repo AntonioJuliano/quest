c8b303f431fd6c993769579686834a2d80491158
Antonio Juliano <antonio.m.juliano@gmail.com>
2017-05-10 16:49:27 -0700

    make adjustable score params

--- src/lib/contractScorer.js
+++ src/lib/contractScorer.js
@@ -1,14 +1,18 @@
+const OLD_SCORE_WEIGHT = parseFloat(process.env.CONTRACT_SCORE_OLD_SCORE_WEIGHT);
+const LINK_WEIGHT = parseFloat(process.env.CONTRACT_SCORE_LINK_WEIGHT);
+const SOURCE_WEIGHT = parseFloat(process.env.CONTRACT_SCORE_SOURCE_WEIGHT);
+
 function score(contract, txCount) {
   // This score is weighted by a log function when used to rank results
   const oldScore = contract.score.value || 0;
 
   // Weight contracts with approved links higher
-  const linkWeight = contract.link ? 1.2 : .8;
+  const linkWeight = contract.link ? LINK_WEIGHT : 1;
 
   // Weight contracts with source code higher
-  const sourceWeight = contract.link ? 1.3 : .7;
+  const sourceWeight = contract.link ? SOURCE_WEIGHT : 1;
 
-  const newScore = Math.round((oldScore * .8 + txCount) * linkWeight * sourceWeight);
+  const newScore = Math.round((oldScore * OLD_SCORE_WEIGHT + txCount) * linkWeight * sourceWeight);
   return newScore;
 }
 
